a23e0625e2714f14f37a0597a5fbab89cc68a078
Avi Khandakar <avi@Avis-MacBook-Air.local>
2022-05-29 23:08:02 +0600

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Fragment } from "react";
+import { Toaster } from "react-hot-toast";
+import DefaultLayout from "layouts/defaults";
+import { AnimatePresence } from "framer-motion";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, router }) {
+  const Layout = Component.layout || DefaultLayout;
+  const Title = Component.title || null;
+
+  return (
+    <Fragment>
+      <AnimatePresence initial={true} exitBeforeEnter>
+        <Layout title={Title} key={router.route}>
+          <main className="min-h-screen">
+            <Component {...pageProps} />
+          </main>
+          <Toaster />
+        </Layout>
+      </AnimatePresence>
+    </Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
